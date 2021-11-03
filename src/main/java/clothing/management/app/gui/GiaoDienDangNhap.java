a33bd27eebddfed08e262062a764aa4803405ea0
Duc Toan Vo <hey.toan@outlook.com>
2021-11-04 00:42:54 +0700

    Revert to login function

--- src/main/java/clothing/management/app/gui/GiaoDienDangNhap.java
+++ src/main/java/clothing/management/app/gui/GiaoDienDangNhap.java
@@ -1,11 +1,11 @@
 package clothing.management.app.gui;
 
-import clothing.management.app.gui.GiaoDienDieuKhien;
+import clothing.management.dao.TaiKhoanDao;
+import com.mongodb.reactivestreams.client.MongoClients;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 
 public class GiaoDienDangNhap extends JFrame {
 
@@ -20,9 +20,9 @@ public class GiaoDienDangNhap extends JFrame {
     private JPasswordField txtMatKhau;
     private JTextField txtTaiKhoan;
 
+    private TaiKhoanDao taiKhoanDao;
+
     public GiaoDienDangNhap() {
-
-
         pnlChinh = new JPanel();
         pnlBot = new JPanel();
         btnDangNhap = new JButton();
@@ -34,7 +34,7 @@ public class GiaoDienDangNhap extends JFrame {
         pnlTop = new JPanel();
         lblLogo = new JLabel();
 
-        setTitle("�?ăng Nhập");
+        setTitle("Đăng Nhập");
         setSize(550, 360);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setUndecorated(false);
@@ -47,15 +47,10 @@ public class GiaoDienDangNhap extends JFrame {
 
         lblMatKhau.setText("Mật Khẩu:");
 
-        btnDangNhap.setText("�?ăng Nhập");
+        btnDangNhap.setText("Đăng Nhập");
         btnDangNhap.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent evt) {
-                try {
-					btnDangNhapActionPerformed(evt);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+                btnDangNhapActionPerformed(evt);
             }
         });
 
@@ -163,7 +158,7 @@ public class GiaoDienDangNhap extends JFrame {
                                 .addComponent(lblLogo, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
-
+        taiKhoanDao = new TaiKhoanDao(MongoClients.create());
     }
 
     private void btnThoatActionPerformed(ActionEvent evt) {
@@ -173,10 +168,25 @@ public class GiaoDienDangNhap extends JFrame {
         }
     }
 
-    private void btnDangNhapActionPerformed(ActionEvent evt) throws InterruptedException {
-        new GiaoDienDieuKhien().setVisible(true);
-        setVisible(false);
+    private void btnDangNhapActionPerformed(ActionEvent evt) {
+        String tenDangNhap = txtTaiKhoan.getText();
+        String matKhau = String.valueOf(txtMatKhau.getPassword());
+
+        if (tenDangNhap.isEmpty() || matKhau.isEmpty()) {
+            JOptionPane.showMessageDialog(pnlChinh, "Tên đăng nhập hoặc mật khẩu không hợp lệ");
+            txtTaiKhoan.requestFocus();
+            return;
+        }
+        try {
+            if (taiKhoanDao.dangNhap(tenDangNhap, matKhau)) {
+                setVisible(false);
+                new GiaoDienDieuKhien().setVisible(true);
+                return;
+            }
+            JOptionPane.showMessageDialog(pnlChinh, "Sai mật khẩu hoặc tên đăng nhập");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(pnlChinh, "Lỗi");
+        }
     }
-
-
 }
