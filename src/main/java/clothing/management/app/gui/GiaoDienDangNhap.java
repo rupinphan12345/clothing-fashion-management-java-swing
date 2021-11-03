c7a23d03c1006e2a7527036a55fc8c67fb89d5b6
ductoanvo <hey.toan@outlook.com>
2021-11-03 23:31:47 +0700

    Update GiaoDienDangNhap
    Add login function

--- src/main/java/clothing/management/app/gui/GiaoDienDangNhap.java
+++ src/main/java/clothing/management/app/gui/GiaoDienDangNhap.java
@@ -1,11 +1,11 @@
-package gui;
+package clothing.management.app.gui;
 
-import clothing.management.app.gui.GiaoDienDieuKhien;
+import clothing.management.dao.TaiKhoanDao;
+import com.mongodb.reactivestreams.client.MongoClients;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 
 public class GiaoDienDangNhap extends JFrame {
 
@@ -20,9 +20,9 @@ public class GiaoDienDangNhap extends JFrame {
     private JPasswordField txtMatKhau;
     private JTextField txtTaiKhoan;
 
+    private TaiKhoanDao taiKhoanDao;
+
     public GiaoDienDangNhap() {
-
-
         pnlChinh = new JPanel();
         pnlBot = new JPanel();
         btnDangNhap = new JButton();
@@ -158,7 +158,7 @@ public class GiaoDienDangNhap extends JFrame {
                                 .addComponent(lblLogo, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
-
+        taiKhoanDao = new TaiKhoanDao(MongoClients.create());
     }
 
     private void btnThoatActionPerformed(ActionEvent evt) {
@@ -169,9 +169,24 @@ public class GiaoDienDangNhap extends JFrame {
     }
 
     private void btnDangNhapActionPerformed(ActionEvent evt) {
-        new GiaoDienDieuKhien().setVisible(true);
-        setVisible(false);
+        String tenDangNhap = txtTaiKhoan.getText();
+        String matKhau = String.valueOf(txtMatKhau.getPassword());
+
+        if (tenDangNhap.isEmpty() || matKhau.isEmpty()) {
+            JOptionPane.showMessageDialog(pnlChinh, "Tên đăng nhập hoặc mật khẩu không hợp lệ");
+            txtTaiKhoan.requestFocus();
+            return;
+        }
+        try {
+            if (taiKhoanDao.dangNhap(tenDangNhap, matKhau)) {
+                setVisible(false);
+                new GiaoDienDieuKhien().setVisible(true);
+                return;
+            }
+            JOptionPane.showMessageDialog(pnlChinh, "Sai mật khẩu hoặc tên đăng nhập");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(pnlChinh, "Lỗi");
+        }
     }
-
-
 }
