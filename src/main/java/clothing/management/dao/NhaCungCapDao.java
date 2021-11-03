a87748d2768c6e56e8e95a134490c0571fc736ee
rupinphan12345 <59106335+rupinphan12345@users.noreply.github.com>
2021-11-04 00:23:00 +0700

    Add files via upload

--- src/main/java/clothing/management/dao/NhaCungCapDao.java
+++ src/main/java/clothing/management/dao/NhaCungCapDao.java
@@ -1,5 +1,6 @@
 package clothing.management.dao;
 
+import clothing.management.entity.LoaiSanPham;
 import clothing.management.entity.NhaCungCap;
 import com.google.gson.Gson;
 import com.mongodb.client.model.Filters;
@@ -18,190 +19,229 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class NhaCungCapDao extends AbstractDao {
-    private Gson gson = new Gson();
-    private MongoCollection<Document> nCCCollection;
+	private Gson gson = new Gson();
+	private MongoCollection<Document> nCCCollection;
 
-    public NhaCungCapDao(MongoClient client) {
-        super(client);
-        nCCCollection = db.getCollection("danhsachnhacungcap");
-    }
+	public NhaCungCapDao(MongoClient client) {
+		super(client);
+		nCCCollection = db.getCollection("danhsachnhacungcap");
+	}
 
-    public List<NhaCungCap> getListSupllier() throws InterruptedException {
-        CountDownLatch latch = new CountDownLatch(1);
-        List<NhaCungCap> nhaCungCaps = new ArrayList<>();
+	public List<NhaCungCap> getListSupllier() throws InterruptedException {
+		CountDownLatch latch = new CountDownLatch(1);
+		List<NhaCungCap> nhaCungCaps = new ArrayList<>();
 
-        nCCCollection
-                .find()
-                .subscribe(new Subscriber<Document>() {
+		nCCCollection.find().subscribe(new Subscriber<Document>() {
 
-                    private Subscription s;
+			private Subscription s;
 
-                    @Override
-                    public void onSubscribe(Subscription s) {
-                        this.s = s;
-                        this.s.request(1);
-                    }
+			@Override
+			public void onSubscribe(Subscription s) {
+				this.s = s;
+				this.s.request(1);
+			}
 
-                    @Override
-                    public void onNext(Document t) {
-                        String json = t.toJson();
-                        NhaCungCap nhaCungCap = gson.fromJson(json, NhaCungCap.class);
+			@Override
+			public void onNext(Document t) {
+				String json = t.toJson();
+				NhaCungCap nhaCungCap = gson.fromJson(json, NhaCungCap.class);
 
-                        nhaCungCaps.add(nhaCungCap);
-                        this.s.request(1);
-                    }
+				nhaCungCaps.add(nhaCungCap);
+				this.s.request(1);
+			}
 
-                    @Override
-                    public void onError(Throwable t) {
-                        t.printStackTrace();
-                        onComplete();
-                    }
+			@Override
+			public void onError(Throwable t) {
+				t.printStackTrace();
+				onComplete();
+			}
 
-                    @Override
-                    public void onComplete() {
-                        latch.countDown();
-                    }
-                });
+			@Override
+			public void onComplete() {
+				latch.countDown();
+			}
+		});
 
-        latch.await();
+		latch.await();
 
-        return nhaCungCaps;
-    }
+		return nhaCungCaps;
+	}
 
-    public List<NhaCungCap> searchSupplier(String criteria, String info) throws InterruptedException {
-        CountDownLatch latch = new CountDownLatch(1);
-        List<NhaCungCap> nhaCungCaps = new ArrayList<NhaCungCap>();
-        nCCCollection.find(Filters.eq(criteria, info)).subscribe(new Subscriber<Document>() {
+	public List<NhaCungCap> searchSupplier(String criteria, String info) throws InterruptedException {
+		CountDownLatch latch = new CountDownLatch(1);
+		List<NhaCungCap> nhaCungCaps = new ArrayList<NhaCungCap>();
+		nCCCollection.find(Filters.eq(criteria, info)).subscribe(new Subscriber<Document>() {
 
-            private Subscription s;
+			private Subscription s;
 
-            @Override
-            public void onSubscribe(Subscription s) {
-                this.s = s;
-                this.s.request(1);
-            }
+			@Override
+			public void onSubscribe(Subscription s) {
+				this.s = s;
+				this.s.request(1);
+			}
 
-            @Override
-            public void onNext(Document t) {
-                NhaCungCap cap = gson.fromJson(t.toJson(), NhaCungCap.class);
-                nhaCungCaps.add(cap);
-                this.s.request(1);
+			@Override
+			public void onNext(Document t) {
+				NhaCungCap cap = gson.fromJson(t.toJson(), NhaCungCap.class);
+				nhaCungCaps.add(cap);
+				this.s.request(1);
 
-            }
+			}
 
-            @Override
-            public void onError(Throwable t) {
-                t.printStackTrace();
-            }
+			@Override
+			public void onError(Throwable t) {
+				t.printStackTrace();
+			}
 
-            @Override
-            public void onComplete() {
-                latch.countDown();
-            }
-        });
-        latch.await();
-        return nhaCungCaps;
+			@Override
+			public void onComplete() {
+				latch.countDown();
+			}
+		});
+		latch.await();
+		return nhaCungCaps;
 
-    }
+	}
 
-    public Boolean updateSuppiler(String nccID, NhaCungCap nhaCungCap) throws InterruptedException {
-        CountDownLatch latch = new CountDownLatch(1);
-        AtomicBoolean bl = new AtomicBoolean(false);
-        String json = gson.toJson(nhaCungCap);
+	public Boolean updateSuppiler(String nccID, NhaCungCap nhaCungCap) throws InterruptedException {
+		CountDownLatch latch = new CountDownLatch(1);
+		AtomicBoolean bl = new AtomicBoolean(false);
+		String json = gson.toJson(nhaCungCap);
 
-        nCCCollection.replaceOne(Filters.eq("maNhaCungCap", nccID), Document.parse(json))
-                .subscribe(new Subscriber<UpdateResult>() {
+		nCCCollection.replaceOne(Filters.eq("maNhaCungCap", nccID), Document.parse(json))
+				.subscribe(new Subscriber<UpdateResult>() {
 
-                    @Override
-                    public void onSubscribe(Subscription s) {
-                        s.request(1);
-                    }
+					@Override
+					public void onSubscribe(Subscription s) {
+						s.request(1);
+					}
 
-                    @Override
-                    public void onNext(UpdateResult t) {
-                        if (t.getModifiedCount() > 0) {
-                            bl.set(true);
-                        }
-                    }
+					@Override
+					public void onNext(UpdateResult t) {
+						if (t.getModifiedCount() > 0) {
+							bl.set(true);
+						}
+					}
 
-                    @Override
-                    public void onError(Throwable t) {
-                        t.printStackTrace();
-                    }
+					@Override
+					public void onError(Throwable t) {
+						t.printStackTrace();
+					}
 
-                    @Override
-                    public void onComplete() {
-                        latch.countDown();
-                    }
-                });
-        latch.await();
-        return bl.get();
-    }
+					@Override
+					public void onComplete() {
+						latch.countDown();
+					}
+				});
+		latch.await();
+		return bl.get();
+	}
 
-    public boolean addSuppiler(NhaCungCap nhaCungCap) throws InterruptedException {
-        CountDownLatch latch = new CountDownLatch(1);
-        AtomicBoolean bl = new AtomicBoolean(false);
-        String json = gson.toJson(nhaCungCap);
-        Document doc = Document.parse(json);
+	public boolean addSuppiler(NhaCungCap nhaCungCap) throws InterruptedException {
+		CountDownLatch latch = new CountDownLatch(1);
+		AtomicBoolean bl = new AtomicBoolean(false);
+		String json = gson.toJson(nhaCungCap);
+		Document doc = Document.parse(json);
 
-        nCCCollection.insertOne(doc).subscribe(new Subscriber<InsertOneResult>() {
+		nCCCollection.insertOne(doc).subscribe(new Subscriber<InsertOneResult>() {
 
-            @Override
-            public void onSubscribe(Subscription s) {
-                s.request(1);
-            }
+			@Override
+			public void onSubscribe(Subscription s) {
+				s.request(1);
+			}
 
-            @Override
-            public void onNext(InsertOneResult t) {
-                if (t.getInsertedId() != null) {
-                    bl.set(true);
-                }
-            }
+			@Override
+			public void onNext(InsertOneResult t) {
+				if (t.getInsertedId() != null) {
+					bl.set(true);
+				}
+			}
 
-            @Override
-            public void onError(Throwable t) {
-                t.printStackTrace();
-            }
+			@Override
+			public void onError(Throwable t) {
+				t.printStackTrace();
+			}
 
-            @Override
-            public void onComplete() {
-                latch.countDown();
-            }
-        });
-        latch.await();
-        return bl.get();
-    }
+			@Override
+			public void onComplete() {
+				latch.countDown();
+			}
+		});
+		latch.await();
+		return bl.get();
+	}
 
-    public boolean deleteSuppiler(String nccId) throws InterruptedException {
-        CountDownLatch latch = new CountDownLatch(1);
-        AtomicBoolean bl = new AtomicBoolean(false);
-        nCCCollection.deleteOne(Filters.eq("maNhaCungCap", nccId)).subscribe(new Subscriber<DeleteResult>() {
+	public boolean deleteSuppiler(String nccId) throws InterruptedException {
+		CountDownLatch latch = new CountDownLatch(1);
+		AtomicBoolean bl = new AtomicBoolean(false);
+		nCCCollection.deleteOne(Filters.eq("maNhaCungCap", nccId)).subscribe(new Subscriber<DeleteResult>() {
 
-            @Override
-            public void onSubscribe(Subscription s) {
-                s.request(1);
-            }
+			@Override
+			public void onSubscribe(Subscription s) {
+				s.request(1);
+			}
 
-            @Override
-            public void onNext(DeleteResult t) {
-                if (t.getDeletedCount() != 0) {
-                    bl.set(true);
-                }
-            }
+			@Override
+			public void onNext(DeleteResult t) {
+				if (t.getDeletedCount() != 0) {
+					bl.set(true);
+				}
+			}
 
-            @Override
-            public void onError(Throwable t) {
-                t.printStackTrace();
-                onComplete();
-            }
+			@Override
+			public void onError(Throwable t) {
+				t.printStackTrace();
+				onComplete();
+			}
 
-            @Override
-            public void onComplete() {
-                latch.countDown();
-            }
+			@Override
+			public void onComplete() {
+				latch.countDown();
+			}
 
-        });
-        latch.await();
-        return bl.get();
-    }
+		});
+		latch.await();
+		return bl.get();
+	}
+
+	public List<NhaCungCap> timTHeoTenNCC(String maNhaCungCap) throws InterruptedException {
+
+		CountDownLatch latch = new CountDownLatch(1);
+		List<NhaCungCap> dsNhaCC = new ArrayList<NhaCungCap>();
+		Document doc = Document.parse("{'maNhaCungCap':'" + maNhaCungCap + "'}");
+		nCCCollection.find(doc).subscribe(new Subscriber<Document>() {
+			private Subscription s;
+
+			@Override
+			public void onSubscribe(Subscription s) {
+				this.s = s;
+				this.s.request(1);
+			}
+
+			@Override
+			public void onNext(Document t) {
+
+				String json = t.toJson();
+				NhaCungCap nhaCC = gson.fromJson(json, NhaCungCap.class);
+				dsNhaCC.add(nhaCC);
+				this.s.request(1);
+			}
+
+			@Override
+			public void onError(Throwable t) {
+				t.printStackTrace();
+				onComplete();
+
+			}
+
+			@Override
+			public void onComplete() {
+				latch.countDown();
+			}
+		});
+
+		latch.await();
+
+		return dsNhaCC;
+	}
 }
