5b2c32c0d8d0c4482d55b571b6f79ee760cd970a
rupinphan12345 <59106335+rupinphan12345@users.noreply.github.com>
2021-11-03 23:07:33 +0700

    Add files via upload

--- src/main/java/clothing/management/dao/HoaDonDao.java
+++ src/main/java/clothing/management/dao/HoaDonDao.java
@@ -4,6 +4,8 @@ import clothing.management.entity.ChiTietHoaDon;
 import clothing.management.entity.HoaDon;
 import clothing.management.entity.KhachHang;
 import clothing.management.entity.NhanVien;
+import clothing.management.entity.SanPham;
+
 import com.google.gson.Gson;
 import com.mongodb.client.model.Filters;
 import com.mongodb.client.result.DeleteResult;
@@ -11,6 +13,7 @@ import com.mongodb.client.result.InsertOneResult;
 import com.mongodb.client.result.UpdateResult;
 import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoCollection;
+
 import org.bson.Document;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
@@ -18,11 +21,15 @@ import org.reactivestreams.Subscription;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 import java.util.TimeZone;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class HoaDonDao extends AbstractDao {
     private Gson gson = new Gson();
@@ -33,59 +40,80 @@ public class HoaDonDao extends AbstractDao {
         hoadonCollection = db.getCollection("danhsachhoadon");
     }
 
-    //	db.danhsachhoadon.aggregate([{$project:{_id:1,giamGia:1,tongTienHoaDon:1,maHoaDon:1,maKhachHang:1,maNhanVien:1,danhSachChiTietHoaDon:1,nam:{$year:"$ngayTao"},thang:{$month:"$ngayTao"},ngay:{$dayOfMonth:"$ngayTao"},gio:{$hour:"$ngayTao"},phut:{$minute:"$ngayTao"},giay:{$second:"$ngayTao"}}}])
-    public List<HoaDon> getAllListBill() throws InterruptedException {
-        CountDownLatch latch = new CountDownLatch(1);
-        List<HoaDon> hoaDons = new ArrayList<HoaDon>();
-        Document doc = Document.parse("{$project:{_id:1,giamGia:1,tongTienHoaDon:1,maHoaDon:1,maKhachHang:1,maNhanVien:1,danhSachChiTietHoaDon:1,nam:{$year:\"$ngayTao\"},thang:{$month:\"$ngayTao\"},ngay:{$dayOfMonth:\"$ngayTao\"},gio:{$hour:\"$ngayTao\"},phut:{$minute:\"$ngayTao\"},giay:{$second:\"$ngayTao\"}}}");
+//	db.danhsachhoadon.aggregate([{$project:{_id:1,giamGia:1,tongTienHoaDon:1,maHoaDon:1,maKhachHang:1,maNhanVien:1,danhSachChiTietHoaDon:1,nam:{$year:"$ngayTao"},thang:{$month:"$ngayTao"},ngay:{$dayOfMonth:"$ngayTao"},gio:{$hour:"$ngayTao"},phut:{$minute:"$ngayTao"},giay:{$second:"$ngayTao"}}}])
+	public List<HoaDon> getAllListBill() throws InterruptedException {
+		CountDownLatch latch = new CountDownLatch(1);
+		List<HoaDon> hoaDons = new ArrayList<HoaDon>();
+		Document doc = Document.parse(
+				"{$project:{_id:1,giamGia:1,tongTienHoaDon:1,maHoaDon:1,maKhachHang:1,maNhanVien:1,danhSachChiTietHoaDon:1,nam:{$year:\"$ngayTao\"},thang:{$month:\"$ngayTao\"},ngay:{$dayOfMonth:\"$ngayTao\"},gio:{$hour:\"$ngayTao\"},phut:{$minute:\"$ngayTao\"},giay:{$second:\"$ngayTao\"}}}");
 
-        hoadonCollection.aggregate(List.of(doc)).subscribe(new Subscriber<Document>() {
+		hoadonCollection.aggregate(Arrays.asList(doc)).subscribe(new Subscriber<Document>() {
 
-            private Subscription s;
+			private Subscription s;
 
-            @Override
-            public void onSubscribe(Subscription s) {
-                this.s = s;
-                this.s.request(1);
-            }
+			@Override
+			public void onSubscribe(Subscription s) {
+				this.s = s;
+				this.s.request(1);
+			}
 
-            @SuppressWarnings({"deprecation", "unchecked"})
-            @Override
-            public void onNext(Document t) {
-                String json = t.toJson();
-                HoaDon hoaDon = gson.fromJson(json, HoaDon.class);
+			@SuppressWarnings({ "deprecation", "unchecked" })
+			@Override
+			public void onNext(Document t) {
+				String json = t.toJson();
+				HoaDon hoaDon = gson.fromJson(json, HoaDon.class);
 //				System.out.println(json);
-                Date date = new Date(t.getInteger("nam") - 1900, t.getInteger("thang") - 1, t.getInteger("ngay"), t.getInteger("gio"), t.getInteger("phut"), t.getInteger("giay"));
-                hoaDon.setNgayTao(date);
+				Date date = new Date(t.getInteger("nam") - 1900, t.getInteger("thang") - 1, t.getInteger("ngay"),
+						t.getInteger("gio"), t.getInteger("phut"), t.getInteger("giay"));
+				hoaDon.setNgayTao(date);
 //				hoaDon.setMaHoaDon(t.getString("maHoaDon"));
-                KhachHang khachHang = new KhachHang();
-                khachHang.setMaKhachHang(t.getString("maKhachHang"));
-                hoaDon.setKhachHang(khachHang);
-                NhanVien nhanVien = new NhanVien();
-                nhanVien.setMaNhanVien(t.getString("maNhanVien"));
-                hoaDon.setNhanVien(nhanVien);
-                List<ChiTietHoaDon> a = (List<ChiTietHoaDon>) t.get("danhSachChiTietHoaDon");
+				KhachHang khachHang = new KhachHang();
+				khachHang.setMaKhachHang(t.getString("maKhachHang"));
+				hoaDon.setKhachHang(khachHang);
+				NhanVien nhanVien = new NhanVien();
+				nhanVien.setMaNhanVien(t.getString("maNhanVien"));
+				hoaDon.setNhanVien(nhanVien);
+				List<Document> a = (List<Document>) t.get("danhSachChiTietHoaDon");
+				List<ChiTietHoaDon> chiTietHoaDons = new ArrayList<ChiTietHoaDon>();
+				a.forEach(s -> {
+					SanPham sanPham = new SanPham();
+					sanPham.setMaSanPham(s.getString("maSanPham"));
+					ChiTietHoaDon chiTietHoaDon = new ChiTietHoaDon();
+					chiTietHoaDon.setSanPham(sanPham);
+					chiTietHoaDon.setSoLuong(s.getInteger("soLuong"));
+					chiTietHoaDon.setDonGia(s.getDouble("donGia"));
+					ChiTietHoaDon newChiTietHoaDon = new ChiTietHoaDon(chiTietHoaDon.getSanPham(), chiTietHoaDon.getSoLuong(), chiTietHoaDon.getDonGia());
+					chiTietHoaDons.add(newChiTietHoaDon);
+				});
 
-                hoaDon.setDanhSachChiTietHoaDon(a);
+				hoaDon.setDanhSachChiTietHoaDon(chiTietHoaDons);
+				
+				try {
+					HoaDon newHoaDon = new HoaDon(hoaDon.getMaHoaDon(), hoaDon.getKhachHang(), hoaDon.getNhanVien(), hoaDon.getNgayTao(), hoaDon.getGiamGia(), hoaDon.getDanhSachChiTietHoaDon());
+					hoaDons.add(newHoaDon);
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 
-                hoaDons.add(hoaDon);
-                this.s.request(1);
-            }
+				
+				this.s.request(1);
+			}
 
-            @Override
-            public void onError(Throwable t) {
-                t.printStackTrace();
-            }
+			@Override
+			public void onError(Throwable t) {
+				t.printStackTrace();
+			}
 
-            @Override
-            public void onComplete() {
-                System.out.println("done!!");
-                latch.countDown();
-            }
-        });
-        latch.await();
-        return hoaDons;
-    }
+			@Override
+			public void onComplete() {
+				System.out.println("done!!");
+				latch.countDown();
+			}
+		});
+		latch.await();
+		return hoaDons;
+	}
 
     public boolean createBill(HoaDon hoaDon) throws InterruptedException, ParseException {
         CountDownLatch latch = new CountDownLatch(1);
@@ -245,4 +273,169 @@ public class HoaDonDao extends AbstractDao {
         latch.await();
         return rs.get();
     }
+    
+    public long layTongSoHoaDon() throws InterruptedException {
+    	
+    	CountDownLatch latch = new CountDownLatch(1);
+    	AtomicLong rs = new AtomicLong(0);
+	
+		hoadonCollection
+		.find()
+		.subscribe(new Subscriber<Document>() {
+			
+			private Subscription s;
+			private long count = 0;
+
+			@Override
+			public void onSubscribe(Subscription s) {
+				this.s = s;
+				this.s.request(1);
+			}
+
+			@Override
+			public void onNext(Document t) {
+				count ++;
+				rs.set(count);
+				this.s.request(1);
+			}
+
+			@Override
+			public void onError(Throwable t) {
+				t.printStackTrace();
+				onComplete();
+			}
+
+			@Override
+			public void onComplete() {
+				latch.countDown();
+			}
+		});
+		
+		latch.await();
+		return rs.get();
+    }
+    
+    
+    
+    
+//  db.danhsachhoadon.aggregate([{ $match: { "maHoaDon": "HD00002" } },
+// { $project: { _id: 1, giamGia: 1, tongTienHoaDon: 1, maHoaDon: 1, maKhachHang: 1, maNhanVien: 1, danhSachChiTietHoaDon: 1, nam: { $year: "$ngayTao" }, thang: { $month: "$ngayTao" }, ngay: { $dayOfMonth: "$ngayTao" }, gio: { $hour: "$ngayTao" }, phut: { $minute: "$ngayTao" }, giay: { $second: "$ngayTao" } } }])
+	 public List<HoaDon> timHoaDon(String criteria, String info) throws InterruptedException {
+	     CountDownLatch latch = new CountDownLatch(1);
+	     List<HoaDon> hoaDons = new ArrayList<HoaDon>();
+	     Document doc1 = Document.parse("{ $match: { "+"\""+criteria+"\""+": "+"\""+info+"\""+" } }");
+	     Document doc2 = Document.parse("{ $project: { _id: 1, giamGia: 1, tongTienHoaDon: 1, maHoaDon: 1, maKhachHang: 1, maNhanVien: 1, danhSachChiTietHoaDon: 1, nam: { $year: \"$ngayTao\" }, thang: { $month: \"$ngayTao\" }, ngay: { $dayOfMonth: \"$ngayTao\" }, gio: { $hour: \"$ngayTao\" }, phut: { $minute: \"$ngayTao\" }, giay: { $second: \"$ngayTao\" } } }");
+	     hoadonCollection.aggregate(Arrays.asList(doc1,doc2)).subscribe(new Subscriber<Document>() {
+	
+	         private Subscription s;
+	
+	         @Override
+	         public void onSubscribe(Subscription s) {
+	             this.s = s;
+	             this.s.request(1);
+	         }
+	
+	         @SuppressWarnings({ "deprecation", "unchecked" })
+				@Override
+	         public void onNext(Document t) {
+	 			String json = t.toJson();
+					HoaDon hoaDon = gson.fromJson(json, HoaDon.class);
+	//				System.out.println(json);
+					Date date = new Date(t.getInteger("nam") - 1900, t.getInteger("thang") - 1, t.getInteger("ngay"),
+							t.getInteger("gio"), t.getInteger("phut"), t.getInteger("giay"));
+					hoaDon.setNgayTao(date);
+	//				hoaDon.setMaHoaDon(t.getString("maHoaDon"));
+					KhachHang khachHang = new KhachHang();
+					khachHang.setMaKhachHang(t.getString("maKhachHang"));
+					hoaDon.setKhachHang(khachHang);
+					NhanVien nhanVien = new NhanVien();
+					nhanVien.setMaNhanVien(t.getString("maNhanVien"));
+					hoaDon.setNhanVien(nhanVien);
+					List<Document> a = (List<Document>) t.get("danhSachChiTietHoaDon");
+					List<ChiTietHoaDon> chiTietHoaDons = new ArrayList<ChiTietHoaDon>();
+					a.forEach(s -> {
+						SanPham sanPham = new SanPham();
+						sanPham.setMaSanPham(s.getString("maSanPham"));
+						ChiTietHoaDon chiTietHoaDon = new ChiTietHoaDon();
+						chiTietHoaDon.setSanPham(sanPham);
+						chiTietHoaDon.setSoLuong(s.getInteger("soLuong"));
+						chiTietHoaDon.setDonGia(s.getDouble("donGia"));
+						ChiTietHoaDon newChiTietHoaDon = new ChiTietHoaDon(chiTietHoaDon.getSanPham(), chiTietHoaDon.getSoLuong(), chiTietHoaDon.getDonGia());
+						chiTietHoaDons.add(newChiTietHoaDon);
+					});
+	
+					hoaDon.setDanhSachChiTietHoaDon(chiTietHoaDons);
+					try {
+						HoaDon newHoaDon = new HoaDon(hoaDon.getMaHoaDon(), hoaDon.getKhachHang(), hoaDon.getNhanVien(), hoaDon.getNgayTao(), hoaDon.getGiamGia(), hoaDon.getDanhSachChiTietHoaDon());
+						hoaDons.add(newHoaDon);
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
+					this.s.request(1);
+	         }
+	
+	         @Override
+	         public void onError(Throwable t) {
+	             t.printStackTrace();
+	         }
+	
+	         @Override
+	         public void onComplete() {
+	             latch.countDown();
+	         }
+	     });
+	     latch.await();
+	     return hoaDons;
+	
+	 }
+	 
+//db.danhsachhoadon.aggregate([{$match: {$expr: {$eq: ["$maHoaDon", "HD00001"]}}},
+//	 							{$unwind: "$danhSachChiTietHoaDon"}, 
+//	 							{$group: {_id: "$maHoaDon", soLuong: {$sum: "$danhSachChiTietHoaDon.soLuong"}}}, 
+//	 							{$project: {maHoaDon: "$_id", soLuongSanPham: "$soLuong"}}])
+	 public int laySoLuongSanPhamTheoMaHoaDon(String maHoaDon) throws InterruptedException {
+		 
+		 CountDownLatch latch = new CountDownLatch(1);
+		 
+		 AtomicInteger rs = new AtomicInteger(0);
+
+		 hoadonCollection.aggregate(Arrays.asList(
+				 Document.parse("{$match: {$expr: {$eq: [\"$maHoaDon\", \""+maHoaDon+"\"]}}}"),
+				 Document.parse("{$unwind: \"$danhSachChiTietHoaDon\"}"),
+				 Document.parse("{$group: {_id: \"$maHoaDon\", soLuong: {$sum: \"$danhSachChiTietHoaDon.soLuong\"}}}"),
+				 Document.parse("{$project: {maHoaDon: \"$_id\", soLuongSanPham: \"$soLuong\"}}")))
+		 .subscribe(new Subscriber<Document>() {
+			 private Subscription s;
+
+			@Override
+			public void onSubscribe(Subscription s) {
+				this.s = s;
+				this.s.request(1);
+			}
+
+			@Override
+			public void onNext(Document t) {
+				rs.set(t.getInteger("soLuongSanPham"));
+				this.s.request(1);
+			}
+
+			@Override
+			public void onError(Throwable t) {
+				t.printStackTrace();
+				onComplete();
+			}
+
+			@Override
+			public void onComplete() {
+				latch.countDown();
+			}
+		});
+		 
+		 latch.await();
+		 
+		 return rs.get();
+		 
+	 }
 }
