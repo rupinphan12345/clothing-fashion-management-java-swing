02ed354175110ada47134f2f7abd484c5b16a200
rupinphan12345 <59106335+rupinphan12345@users.noreply.github.com>
2021-11-04 00:21:44 +0700

    Add files via upload

--- src/main/java/clothing/management/app/ClothingManagementApplication.java
+++ src/main/java/clothing/management/app/ClothingManagementApplication.java
@@ -1,10 +1,11 @@
 package clothing.management.app;
 
+import clothing.management.app.gui.GiaoDienDangNhap;
 import clothing.management.app.gui.GiaoDienDieuKhien;
-import clothing.management.app.gui.GiaoDienHoaDon;
 
 public class ClothingManagementApplication {
     public static void main(String[] args) throws InterruptedException {
-    	new GiaoDienDieuKhien().setVisible(true);
+//    	new GiaoDienDieuKhien().setVisible(true);
+    	new GiaoDienDangNhap().setVisible(true);
     }
 }
